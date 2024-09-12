9c6859d5a50552ede5e3d313601abbab4baf0d69
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-12 20:47:07 -0300

    fIx - substitui o int como pk para serial; substitui decimal para numeric e ajustei precisões desnecessárias

--- sql/Banco.sql
+++ sql/Banco.sql
@@ -1,63 +1,63 @@
 CREATE TABLE Alerta (
-    id int NOT NULL,
+    id serial NOT NULL,
     id_parametro int NOT NULL,
     condicao varchar(4) NOT NULL,
     nome varchar(100) NOT NULL,
-    valor decimal(100, 10) NOT NULL,
+    valor numeric(20, 5) NOT NULL,
     CONSTRAINT Alerta_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Estacao (
-    id int NOT NULL,
+    id serial NOT NULL,
     nome varchar(180) NOT NULL,
     endereco varchar(200) NOT NULL,
-    latitude decimal(3, 6) NOT NULL,
-    longitude decimal(3, 6) NOT NULL,
+    latitude numeric(9, 6) NOT NULL,
+    longitude numeric(9, 6) NOT NULL,
     mac_address varchar(20) NOT NULL,
     CONSTRAINT Estacao_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Medicao (
-    id int NOT NULL,
+    id serial NOT NULL,
     data_hora timestamp NOT NULL,
-    valor_calculado decimal(100, 10) NOT NULL,
+    valor_calculado numeric(20, 5) NOT NULL,
     id_parametro int NOT NULL,
     CONSTRAINT Medicao_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Ocorrencia (
-    id int NOT NULL,
+    id serial NOT NULL,
     id_alerta int NOT NULL,
     data_hora timestamp NOT NULL,
-    valor decimal(100, 10) NOT NULL,
+    valor numeric(20, 5) NOT NULL,
     CONSTRAINT Ocorrencia_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Parametro (
-    id int NOT NULL,
+    id serial NOT NULL,
     id_estacao int NOT NULL,
     id_tipo int NOT NULL,
     CONSTRAINT Parametro_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Tipo_Parametro (
-    id int NOT NULL,
+    id serial NOT NULL,
     id_unidade int NOT NULL,
     nome varchar(100) NOT NULL,
-    fator decimal(100, 10) NOT NULL,
-    "offset" decimal(100, 10) NOT NULL,
+    fator numeric(20, 5) NOT NULL,
+    "offset" numeric(20, 5) NOT NULL,
     nome_json varchar(180) NOT NULL,
     CONSTRAINT Tipo_Parametro_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Unidade_Medida (
-    id int NOT NULL,
+    id serial NOT NULL,
     nome varchar(100) NOT NULL,
     CONSTRAINT Unidade_Medida_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Usuario (
-    id int NOT NULL,
+    id serial NOT NULL,
     nome varchar(100) NOT NULL,
     email varchar(180) NOT NULL,
     senha varchar(180) NOT NULL,
